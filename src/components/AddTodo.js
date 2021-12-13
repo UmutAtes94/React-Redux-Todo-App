4f49542aa1595568f5905a46a4555e2131755d64
Umut Ates <umut.ates@etiya.com>
2021-12-13 20:34:21 +0300

    first commit of app

---
+++ src/components/AddTodo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddTodo = () => {
+    return (
+        <div>
+            <input placeholder="add todo" />
+            <button>Add Todo</button>
+        </div>
+    )
+}
+
+export default AddTodo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TodoList = () => {
+
+    return(
+        <div>
+            <ul>
+                <li>todo 1</li>
+                <li>todo 2</li>
+                <li>todo 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
