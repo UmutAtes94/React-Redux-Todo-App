a2c545582526b69e25ea614a127e6e1cb438dfd3
Umut Ates <umut.ates@etiya.com>
2021-12-13 21:10:10 +0300

    import redux and create store in index js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+//* redux store 
+const store = createStore();
 
 ReactDOM.render(
-    <App />,
+  <Provider store = {store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
